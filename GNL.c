331ac4f2e1acf34fdc933e01156fb55d02b9022d
Janghun Han <janhan@cx1r5s2.42seoul.kr>
2023-10-17 15:45:57 +0900

    fix GNL

--- GNL.c
+++ GNL.c
@@ -1,22 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get _next_line.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: janhan <janhan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/17 13:03:32 by janhan            #+#    #+#             */
-/*   Updated: 2023/10/17 13:30:31 by janhan           ###   ########.fr       */
+/*   Created: 2023/10/16 15:50:03 by janhan            #+#    #+#             */
+/*   Updated: 2023/10/17 12:38:09 by janhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
- #ifndef BUFFER_SIZE
- # define BUFFER_SIZE 1024
- #endif
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1024
+#endif
 
-static char *ft_cutchar(char *line)
+static char	*function_name(int fd, char *buff, char *backup)
+{
+	int		read_line;
+	char	*char_temp;
+
+	read_line = 1;
+	while (read_line != '\0')
+	{
+		read_line = read(fd, buff, BUFFER_SIZE);
+		if (read_line == -1)
+			return (NULL);
+		else if (read_line == 0)
+			break ;
+		buff[read_line] = '\0';
+		if (!backup)
+			backup = ft_strdup("");
+		char_temp = backup;
+		backup = ft_strjoin(char_temp, buff);
+		free(char_temp);
+		char_temp = NULL;
+		if (ft_strchr(buff, '\n'))
+			break ;
+	}
+	return (backup);
+}
+
+static char	*extract(char *line)
 {
 	size_t	count;
 	char	*backup;
@@ -24,7 +50,7 @@ static char *ft_cutchar(char *line)
 	count = 0;
 	while (line[count] != '\n' && line[count] != '\0')
 		count++;
-	if(line[count] == '\0' || line[1] == '\0')
+	if (line[count] == '\0' || line[1] == '\0')
 		return (NULL);
 	backup = ft_substr(line, count + 1, ft_strlen(line) - count);
 	if (*backup == '\0')
@@ -36,53 +62,21 @@ static char *ft_cutchar(char *line)
 	return (backup);
 }
 
-static char *ft_read(int fd, char *buff, char *backup)
-{
-	int		read_line;
-	char	*temp_char;
-
-	read_line = 1;
-	while (read_line != '\0')
-	{
-		if ((read_line = read(fd, buff, BUFFER_SIZE)) == -1)
-			return (NULL);
-		else if (read_line == 0)
-			break ;
-		buff[read_line] = '\0';
-		if (!backup)
-			backup = ft_strdup("");
-		temp_char = backup;
-		backup = ft_strjoin(temp_char, buff);
-		free(temp_char);
-		temp_char = NULL;
-		if(ft_strchr(buff, '\n'))
-			break ;
-	}
-	return (backup);
-}
-
 char	*get_next_line(int fd)
 {
 	char		*line;
 	char		*buff;
 	static char	*backup;
 
-	if ( fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buff)
 		return (NULL);
-	line = ft_read(fd, buff, backup);
+	line = function_name(fd, buff, backup);
 	free(buff);
 	if (!line)
-	{
-		if (backup)
-		{
-			free(backup);
-			backup = NULL;
-		}
 		return (NULL);
-	}
-	backup = ft_cutchar(line);
+	backup = extract(line);
 	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_ling_unils.c                              :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: janhan <janhan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 15:51:27 by janhan            #+#    #+#             */
-/*   Updated: 2023/10/16 22:12:59 by janhan           ###   ########.fr       */
+/*   Updated: 2023/10/17 13:39:57 by janhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 	char	*temp_str;
 
 	temp_str = (char *)malloc(sizeof(char) * (len + 1));
-	if (str == 0)
+	if (str == NULL)
 		return (NULL);
 	i = 0;
 	j = 0;
@@ -85,7 +85,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)));
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (str == NULL)
 		return (NULL);
 	while (s1[i] != '\0')
